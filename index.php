31585a6130bc18b403bd616e9a89587fe32b4a85
Brandious <adnanfacic96@gmail.com>
2019-04-30 21:27:21 +0200

    Fixing bugs

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 
     require_once("classes.php");
     include("views/header.php");
-
+    include("styles.css");
+    
     if($_GET['page'] == 'timeline')
         include("views/timeline.php");
     else if($_GET['page'] == 'yourtweets')
--- script/script.sh
+++ script/script.sh
@@ -1,4 +1,6 @@
 # mysql://b9ff5642ee091a:a01a0c5c@eu-cdbr-west-02.cleardb.net/heroku_1811ddbbc9d6917?reconnect=true
 
+# mysql://b9ff5642ee091a:a01a0c5c@eu-cdbr-west-02.cleardb.net/heroku_1811ddbbc9d6917?reconnect=true
+
 mysql -u b9ff5642ee091a -h eu-cdbr-west-02.cleardb.net -p heroku_1811ddbbc9d6917
  < twitter.sql
