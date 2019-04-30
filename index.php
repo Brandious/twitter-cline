dd896f306bfd8dde6eece6b961238f9f508cecce
Brandious <adnanfacic96@gmail.com>
2019-04-30 21:31:50 +0200

    Fixing bugs

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
     require_once("classes.php");
     include("views/header.php");
-    include("styles.css");
+   
     
     if($_GET['page'] == 'timeline')
         include("views/timeline.php");
